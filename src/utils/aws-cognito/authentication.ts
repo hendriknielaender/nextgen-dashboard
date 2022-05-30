a53ea07d6301900d4159cb2731dfb4f7acd43b66
hendrik.nielaender@taxdoo.com <hendrik.nielaender@taxdoo.com>
2022-05-30 23:01:49 +0200

    feat(cognito-local): add authentication func

---
+++ src/utils/aws-cognito/authentication.ts
@@ -0,0 +1,89 @@
+import 'cross-fetch';
+import {
+  CognitoUser,
+  AuthenticationDetails,
+  CognitoUserPool,
+} from 'amazon-cognito-identity-js';
+import * as AWS from 'aws-sdk/global';
+
+const USERPOOL_ID = 'local_4y9WovNG';
+const IDENTITY_POOL_ID = 'local';
+
+const userPool = new CognitoUserPool({
+  endpoint: 'http://localhost:9229/',
+  UserPoolId: 'local_4y9WovNG',
+  ClientId: '1krqt04ofu2v3fuw5ntxqog58',
+});
+
+export function signInUser({ email, password }: any) {
+  return new Promise((resolve, reject) => {
+    const authenticationDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password,
+    });
+
+    const cognitoUser = new CognitoUser({
+      Username: email,
+      Pool: userPool,
+    });
+
+    authenticateUser(cognitoUser, authenticationDetails)
+      .then((userProfileObject) => {
+        console.debug(`userProfileObject ${userProfileObject}`);
+        resolve(userProfileObject);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+function authenticateUser(
+  cognitoUser: CognitoUser,
+  authenticationDetails: AuthenticationDetails,
+) {
+  return new Promise<void>((resolve, reject) => {
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+        localStorage.setItem(
+          'user_token',
+          result.getAccessToken().getJwtToken(),
+        );
+
+        //POTENTIAL: Region needs to be set if not already set previously elsewhere.
+        AWS.config.region = 'local';
+
+        const loginsObj = {
+          [USERPOOL_ID]: result.getIdToken().getJwtToken(),
+        };
+
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: IDENTITY_POOL_ID,
+          Logins: {
+            // Change the key below according to the specific region your user pool is in.
+            local_4y9WovNG: result.getIdToken().getJwtToken(),
+          },
+        });
+
+        //refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
+        (<AWS.CognitoIdentityCredentials>AWS.config.credentials).refresh(
+          (error) => {
+            if (error) {
+              console.error(error);
+            } else {
+              // Instantiate aws sdk service objects now that the credentials have been updated.
+              // example: var s3 = new AWS.S3();
+              console.log('Successfully logged!');
+            }
+          },
+        );
+
+        resolve();
+      },
+      onFailure: function (err) {
+        console.error(err);
+        reject(err);
+      },
+    });
+  });
+}
